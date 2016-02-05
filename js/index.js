fc0cd3da91f14d43183b3b9d0712627493ea2f89
nicolas-mosch <nicolas.morew@live.com>
2016-02-05 08:51:07 +0100

    A

--- js/index.js
+++ js/index.js
@@ -1,29 +1,395 @@
 var d3 = require('d3');
+var ipc = require("electron").ipcRenderer;
+var _ = require('lodash/core');
+var hljs = require('highlight.js');
 var dagreD3 = require('dagre-d3');
-var ipc = require("electron").ipcMain;
-var xml2js = require('xml2js');
-var fs = require('fs');
-var graphGenerator = require('./node_modules/graphGenerator/graphGenerator');
-var g;
+var data = null;
+var editors = [];
+var range = 0;
+
+$(document).ready(function() {
+  $("#legendButton").click(function() {
+    ipc.send('showLegend', 'test');
+  });
+});
+
+
+ipc.on('alert', function(event, message) {
+  alert(message);
+});
+
+ipc.on('setData', function(event, dat) {
+  data = {};
+  _.forEach(dat, function(value, key) {
+    data[value.id] = value;
+  });
+})
+
+ipc.on('renderGraph1', function(event, message) {
+  JSON.stringify(data, null, 2)
+  var g = new dagreD3.graphlib.Graph({
+      compound: true
+    })
+    .setGraph({})
+    .setDefaultEdgeLabel(function() {
+      return {};
+    });
+
+  _.forEach(data, function(value, key) {
+    data[value.id] = value;
+    var fillColor;
+    switch (value.type) {
+      case 0:
+        fillColor = "#00FF00";
+        break;
+      case 1:
+        fillColor = "#00FFFF";
+        break;
+      case 2:
+        fillColor = "#FF6633";
+        break;
+      case 3:
+        fillColor = "#FFFF33";
+        break;
+      default:
+        fillColor = "";
+    }
+    g.setNode(value.id, {
+      label: value.id,
+      width: 144,
+      height: 100,
+      style: "fill: " + fillColor,
+      description: value.name,
+      id: value.id
+    });
+
+  });
+
+  _.forEach(data, function(value1, key1) {
+    _.forEach(value1.childrenNodes, function(value2, key2) {
+      g.setEdge(value1.id, value2, {
+        id: value1.id + "-" + value2
+      });
+    });
+  });
+
+  g.nodes().forEach(function(v) {
+    var node = g.node(v);
+    // Round the corners of the nodes
+    node.rx = node.ry = 5;
+  });
+  var render = new dagreD3.render();
+
+  // Set up an SVG group so that we can translate the final graph.
+  var svg = d3.select("svg");
+  svg.selectAll("*").remove();
+  var inner = svg.append("g");
+  // Set up zoom support
+  var zoom = d3.behavior.zoom().on("zoom", function() {
+    inner.attr("transform", "translate(" + d3.event.translate + ")" +
+      "scale(" + d3.event.scale + ")");
+  });
+  svg.call(zoom);
+
+
+  // Run the renderer. This is what draws the final graph.
+  render(inner, g);
+
+  // Set tooltips
+  var styleTooltip = function(name, description) {
+    return "<p class='name'>" + name + "</p><p class='description'>" + description + "</p>";
+  };
+  inner.selectAll("g.node")
+    .attr("title", function(v) {
+      return styleTooltip(v, g.node(v).description)
+    })
+    .each(function(v) {
+      //alert($(this).css('opacity'));
+        $(this).tipsy({
+          gravity: "w",
+          opacity: 1,
+          html: true
+        });
+    });
+
+
+  // Set left-click events for nodes
+  inner.selectAll("g.node").on('click', function(d) {
+    var Range = ace.require('ace/range').Range;
+    var start = data[this.id].start.split(':');
+    var end = data[this.id].end.split(':');
+    if (range)
+      editors[4].removeSelectionMarker(range);
+    range = new Range(start[1] - 1, start[0] - 1, end[1] - 1, end[0] - 1);
+    editors[4].addSelectionMarker(
+      range
+    );
+    $('#node-info').text( JSON.stringify(data[this.id], null, 2) );
+  });
+
+
+  // Set right-click events for nodes
+  function hideChildren(node) {
+    _.forEach(node.childrenNodes, function(value, key) {
+      hideChildren(data[value]);
+      var childNode = d3.select('[id="' + value + '"]');
+      var edge = d3.select('[id="' + node.id + '-' + value + '"]');
+
+      if (childNode.style('display') == 'inline') {
+        childNode.style("display", 'none');
+      } else {
+        childNode.style("display", 'inline');
+      }
+
+      if (edge.style('display') == 'inline') {
+        edge.style("display", 'none');
+      } else {
+        edge.style("display", 'inline');
+      }
+
+
+
+    });
+  };
+
+  inner.selectAll("g.node").on('contextmenu', function(d) {
+    hideChildren(data[this.id]);
+  });
+
+
+});
+
+
+function getTree(root) {
+  var tree = {
+    "name": root.id,
+    "children": []
+  };
+  //alert("CurrentNode: " + JSON.stringify(root, null, 2));
+  _.forEach(root.childrenNodes, function(value, key) {
+    //alert('adding' + value);
+    tree["children"].push(getTree(data[value]));
+    //alert('added ' + value);
+  });
+  if (tree["children"].length == 0)
+    delete tree["children"];
+  //alert("Returning: " + JSON.stringify(tree, null, 2));
+  return tree;
+}
+
+ipc.on('renderGraph2', function(event, message) {
+  var pubs = getTree(data["1:12"]);
+  var diameter = 800;
+
+  var margin = {
+      top: 20,
+      right: 120,
+      bottom: 20,
+      left: 120
+    },
+    width = diameter,
+    height = diameter;
+
+  var i = 0,
+    duration = 350,
+    root;
+
+  var tree = d3.layout.tree()
+    .size([360, diameter / 2 - 80])
+    .separation(function(a, b) {
+      return (a.parent == b.parent ? 1 : 10) / a.depth;
+    });
+
+  var diagonal = d3.svg.diagonal.radial()
+    .projection(function(d) {
+      return [d.y, d.x / 180 * Math.PI];
+    });
+
+  var drag = d3.behavior.drag();
+  drag.on("dragstart", function() {
+    alert('dragging');
+    d3.event.sourceEvent.stopPropagation(); // silence other listeners
+  });
+
+
+  var svg = d3.select("svg").call(d3.behavior.zoom().on("zoom", function() {
+      svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+    }))
+    .append("g");
+
+
+
+
+  root = pubs;
+  root.x0 = height / 2;
+  root.y0 = 0;
+
+  //root.children.forEach(collapse); // start with all children collapsed
+  update(root);
+
+  //d3.select(self.frameElement).style("height", "800px");
+
+  function update(source) {
+
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root),
+      links = tree.links(nodes);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) {
+      d.y = d.depth * 80;
+    });
+
+    // Update the nodes…
+    var node = svg.selectAll("g.node")
+      .data(nodes, function(d) {
+        return d.id || (d.id = ++i);
+      });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+      .attr("class", "node")
+      //.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+      .on("click", click);
+
+    nodeEnter.append("circle")
+      .attr("r", 1e-6)
+      .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+      });
+
+    nodeEnter.append("text")
+      .attr("x", 10)
+      .attr("dy", ".35em")
+      .attr("text-anchor", "start")
+      //.attr("transform", function(d) { return d.x < 180 ? "translate(0)" : "rotate(180)translate(-" + (d.name.length * 8.5)  + ")"; })
+      .text(function(d) {
+        return d.name;
+      })
+      .style("fill-opacity", 1e-6);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+      .duration(duration)
+      .attr("transform", function(d) {
+        return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+      })
+
+    nodeUpdate.select("circle")
+      .attr("r", 4.5)
+      .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+      });
+
+    nodeUpdate.select("text")
+      .style("fill-opacity", 1)
+      .attr("transform", function(d) {
+        return d.x < 180 ? "translate(0)" : "rotate(180)translate(-" + (d.name.length + 50) + ")";
+      });
+
+    // TODO: appropriate transform
+    var nodeExit = node.exit().transition()
+      .duration(duration)
+      //.attr("transform", function(d) { return "diagonal(" + source.y + "," + source.x + ")"; })
+      .remove();
+
+    nodeExit.select("circle")
+      .attr("r", 1e-6);
+
+    nodeExit.select("text")
+      .style("fill-opacity", 1e-6);
+
+    // Update the links…
+    var link = svg.selectAll("path.link")
+      .data(links, function(d) {
+        return d.target.id;
+      });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+      .attr("class", "link")
+      .attr("d", function(d) {
+        var o = {
+          x: source.x0,
+          y: source.y0
+        };
+        return diagonal({
+          source: o,
+          target: o
+        });
+      });
+
+    // Transition links to their new position.
+    link.transition()
+      .duration(duration)
+      .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+      .duration(duration)
+      .attr("d", function(d) {
+        var o = {
+          x: source.x,
+          y: source.y
+        };
+        return diagonal({
+          source: o,
+          target: o
+        });
+      })
+      .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+
+    update(d);
+  }
+
+  // Collapse nodes
+  function collapse(d) {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(collapse);
+      d.children = null;
+    }
+  }
+
+
+});
+
+
+
+
+
 
 
 $(function() {
-
   var onSampleResized = function(e) {
     var columns = $(e.currentTarget).find("td");
   };
-
-
   $("#table").colResizable();
-
 })
 
-
-
 function initAce() {
-
-  $('.editor').each(function(i, obj) {
-    var editor = ace.edit(obj);
+  var i = 0;
+  $('code-element').each(function(i, obj) {
+    var container = obj.querySelector('div');
+    var editor = ace.edit(container);
+    editors[i] = editor;
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/c_cpp");
     editor.setReadOnly(true);
@@ -31,21 +397,17 @@ function initAce() {
     editor.setOptions({
       maxLines: 1000
     });
+    i++;
   });
 }
 
 
-function renderGraph(path) {
-  graphGenerator.buildGraphFromXML(path);
-  alert('test');
-}
-
-
-function testProgressBar(){
+function testProgressBar() {
   var elem = document.getElementById("progress-bar");
 
   var width = 0;
   var id = setInterval(frame, 70);
+
   function frame() {
     if (width == 100) {
       clearInterval(id);
@@ -59,15 +421,21 @@ function testProgressBar(){
   //$('#progress-bar').css("display", "none");
 }
 
-function loadTestGraph() {
 
-  g = new dagreD3.graphlib.Graph({
+
+
+
+/*
+function renderTestGraph() {
+
+  var g = new dagreD3.graphlib.Graph({
       compound: true
     })
     .setGraph({})
     .setDefaultEdgeLabel(function() {
       return {};
     });
+
   // Here we're setting the nodes
   g.setNode('a', {
     label: 'A'
@@ -129,16 +497,10 @@ function loadTestGraph() {
     // Round the corners of the nodes
     node.rx = node.ry = 5;
   });
-
-}
-
-
-function renderGraph() {
-  loadTestGraph();
   var render = new dagreD3.render();
 
   // Set up an SVG group so that we can translate the final graph.
-  var svg = d3.select("svg");
+  var svg = d3.select("#graph-container").append("svg").attr("width", 800).attr("height", 800);
   svg.selectAll("*").remove()
   var inner = svg.append("g");
 
@@ -157,9 +519,6 @@ function renderGraph() {
     alert(d);
   });
 
-}
 
-module.exports = {
-  loadTestGraph: loadTestGraph,
-  buildGraphFromXML: buildGraphFromXML
-};
+}
+*/
--- main.js
+++ main.js
@@ -2,11 +2,15 @@ var app = require('app'); // Module to control application life.
 var BrowserWindow = require('browser-window'); // Module to create native browser window.
 var Menu = require('menu');
 var MenuItem = require('menu-item');
-//var ipc = require("electron").ipcMain;
+var ipc = require("electron").ipcMain;
 var graphGenerator = require('./node_modules/graphGenerator/graphGenerator');
+var fs = require('fs');
+var _ = require('lodash/core');
+var legendWindow = null;
+
 var mainWindow = null;
 
-//var ipc = require('ipc');
+
 var template = [{
     label: 'File',
     submenu: [{
@@ -19,18 +23,22 @@ var template = [{
           title: 'Import',
           defaultPath: '/home',
           filters: [{
-            name: 'XML',
-            extensions: ['xml']
+            name: 'JSON',
+            extensions: ['json']
           }],
           properties: ['openFile']
         });
 
-        if(filePaths == null)
+        if (filePaths == null)
           return;
+        var fileContents = fs.readFileSync(filePaths[0]);
 
-        //TODO: build graph from data
 
-        mainWindow.webContents.executeJavaScript("renderGraph('"+filePaths[0]+"');");
+
+        //        console.log(JSON.stringify(data, null, 2));
+
+        mainWindow.webContents.send('setData', JSON.parse(fileContents));
+        mainWindow.webContents.send('renderGraph1');
       }
     }]
   }, {
@@ -39,8 +47,11 @@ var template = [{
       label: 'Graph',
       role: 'graphType',
       submenu: [{
-        label: 'Dependeny Wheel',
-        role: 'dependencyWheel',
+        label: 'Radial Tree',
+        role: 'radialTree',
+        click: function() {
+          mainWindow.webContents.send('renderGraph2');
+        }
       }]
     }, {
       label: 'Data',
@@ -55,16 +66,14 @@ var template = [{
         label: 'File Mapping',
         role: 'fileMapping',
       }]
-    },
-    {
+    }, {
       label: 'Code',
       role: 'codeView',
       submenu: [{
         label: 'Tabbed',
         role: 'All',
       }]
-    }
-  ]
+    }]
   }
 
 ];
@@ -74,6 +83,23 @@ require('crash-reporter').start();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
+ipc.on('showLegend', function(event, arg) {
+  if (legendWindow){
+    legendWindow.close();
+    return;
+  }
+  legendWindow = new BrowserWindow({
+    width: 400,
+    height: 600
+  });
+  legendWindow.setMenu(null);
+  legendWindow.setAlwaysOnTop(true);
+  legendWindow.loadURL('file://' + __dirname + '/legends/' + arg + '.html');
+  legendWindow.on('closed', function () {
+        legendWindow = null;
+    });
+
+});
 
 
 // Quit when all windows are closed.
@@ -96,15 +122,13 @@ app.on('ready', function() {
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.maximize();
+  //mainWindow.webContents.openDevTools();
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
+    legendWindow = null;
     mainWindow = null;
   });
 });
-
-//ipc.on('close-main-window', function () {
-//    app.quit();
-//});
